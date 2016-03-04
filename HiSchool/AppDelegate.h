c46f9dca459bf3943de8f4859ca78266732f5578
ybon <ybon@ybondeimac-4.local>
2016-03-04 13:05:39 +0800

    Initial Commit

---
+++ HiSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiSchool
+//
+//  Created by ybon on 16/3/4.
+//  Copyright © 2016年 ybon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HiSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiSchool
+//
+//  Created by ybon on 16/3/4.
+//  Copyright © 2016年 ybon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
